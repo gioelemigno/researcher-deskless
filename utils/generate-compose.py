a818053eaba9aaf6fda27e6f33c2f67c02642da7
Gioele Migno <migno.gioele@gmail.com>
2026-05-05 22:16:37 +0200

    small fix

--- utils/generate-compose.py
+++ utils/generate-compose.py
@@ -1,6 +1,7 @@
 import yaml
 import sys
 import os
+from pathlib import Path
 
 with open("config.yaml") as f:
     config = yaml.safe_load(f)
@@ -9,13 +10,17 @@ apps = config.get("apps", [])
 
 includes = ["compose.main.yaml"]
 
+dir_script = Path(__file__).resolve().parent
+
 for app in apps:
     compose_fragment = f"apps/{app}/compose.yaml"
     if not os.path.exists(compose_fragment):
         print(f"ERROR: No compose fragment found for '{app}' at {compose_fragment}")
         sys.exit(1)
     includes.append(compose_fragment)
-    
+    dir_data_app = Path(dir_script / Path(f"../data/{app}")).resolve()
+    os.makedirs(dir_data_app, exist_ok=True)
+
 output = {"include": includes}
 
 with open("compose.yaml", "w") as f:
